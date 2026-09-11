0ff2ee7947416db90f137c64bcba33890f2d97e7
Wade7wastaken <86390605+Wade7wastaken@users.noreply.github.com>
2026-09-11 14:53:49 -0500

    spheres

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, type FC } from "react";
+import { useEffect, useRef, type FC, type RefObject } from "react";
+import { Vector3 } from "three";
 
 import computeShader from "./compute.wgsl?raw";
 
@@ -6,7 +7,10 @@ const fail = (message: string): void => {
   alert(message);
 };
 
-const setupRenderer = async (canvas: HTMLCanvasElement): Promise<void> => {
+const setupRenderer = async (
+  canvas: HTMLCanvasElement,
+  camera_pos: RefObject<Vector3>,
+): Promise<void> => {
   const adapter = await navigator.gpu.requestAdapter();
   if (adapter == undefined) {
     fail("need a browser that supports WebGPU");
@@ -30,20 +34,21 @@ const setupRenderer = async (canvas: HTMLCanvasElement): Promise<void> => {
     device,
     format: "bgra8unorm",
     // This is what's required to be able to write to a texture from a compute shader
-    usage:
-      GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.STORAGE_BINDING,
+    usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.STORAGE_BINDING,
   });
 
   const module = device.createShaderModule({
     code: computeShader,
   });
 
-  const uniformBufferSize = 1 * 4; // 1 Float32 elements * 4 bytes
+  const uniformsNumberFloats = 64;
+
+  const uniformBufferSize = uniformsNumberFloats * 4; // 1 Float32 elements * 4 bytes
   const uniformBuffer = device.createBuffer({
     size: uniformBufferSize,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
-  const uniformData = new Float32Array(1);
+  const uniformData = new Float32Array(uniformsNumberFloats);
 
   const pipeline = device.createComputePipeline({
     label: "checkboard pipeline",
@@ -54,9 +59,60 @@ const setupRenderer = async (canvas: HTMLCanvasElement): Promise<void> => {
     },
   });
 
+  const setUniforms = (
+    uniformData: Float32Array<ArrayBuffer>,
+    canvasWidth: number,
+    canvasHeight: number,
+  ) => {
+    const viewport_height = 2;
+    const viewport_width = viewport_height * (canvasWidth / canvasHeight);
+
+    const focal_length = 1;
+    const camera_center = camera_pos.current.clone();
+
+    const viewport_u = new Vector3(viewport_width, 0, 0);
+    const viewport_v = new Vector3(0, -viewport_height, 0);
+
+    const pixel_delta_u = viewport_u.clone().divideScalar(canvasWidth);
+    const pixel_delta_v = viewport_v.clone().divideScalar(canvasHeight);
+
+    const viewport_upper_left = camera_center
+      .clone()
+      .sub(new Vector3(0, 0, focal_length))
+      .sub(viewport_u.clone().divideScalar(2))
+      .sub(viewport_v.clone().divideScalar(2));
+
+    const pixel00_loc = viewport_upper_left
+      .clone()
+      .add(pixel_delta_u.clone().add(pixel_delta_v).multiplyScalar(0.5));
+
+    uniformData[0] = pixel00_loc.x;
+    uniformData[1] = pixel00_loc.y;
+    uniformData[2] = pixel00_loc.z;
+
+    uniformData[3] = 0;
+
+    uniformData[4] = pixel_delta_u.x;
+    uniformData[5] = pixel_delta_u.y;
+    uniformData[6] = pixel_delta_u.z;
+
+    uniformData[7] = 0;
+
+    uniformData[8] = pixel_delta_v.x;
+    uniformData[9] = pixel_delta_v.y;
+    uniformData[10] = pixel_delta_v.z;
+
+    uniformData[11] = 0;
+
+    uniformData[12] = camera_center.x;
+    uniformData[13] = camera_center.y;
+    uniformData[14] = camera_center.z;
+  };
+
   function render(time: DOMHighResTimeStamp): void {
     time *= 0.001;
-    uniformData[0] = time;
+    uniformData[15] = time;
+    setUniforms(uniformData, canvas.clientWidth, canvas.clientHeight);
     device.queue.writeBuffer(uniformBuffer, 0, uniformData);
 
     // Get the current texture from the canvas context
@@ -85,17 +141,49 @@ const setupRenderer = async (canvas: HTMLCanvasElement): Promise<void> => {
   }
 
   requestAnimationFrame(render);
-}
+};
 
 const App: FC = () => {
   const canvasReference = useRef<HTMLCanvasElement>(null);
+  const camera_pos = useRef<Vector3>(new Vector3(0, 0, 0));
 
   useEffect(() => {
     if (canvasReference.current == undefined) {
       fail("couldn't find canvas");
       return;
     }
-    void setupRenderer(canvasReference.current);
+    void setupRenderer(canvasReference.current, camera_pos);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "w": {
+          camera_pos.current.z -= 0.1;
+          break;
+        }
+        case "s": {
+          camera_pos.current.z += 0.1;
+          break;
+        }
+        case "a": {
+          camera_pos.current.x -= 0.1;
+          break;
+        }
+        case "d": {
+          camera_pos.current.x += 0.1;
+          break;
+        }
+        case " ": {
+          camera_pos.current.y += 0.1;
+          break;
+        }
+        case "Shift": {
+          camera_pos.current.y -= 0.1;
+          break;
+        }
+      }
+    });
   }, []);
 
   return (
--- src/compute.wgsl
+++ src/compute.wgsl
@@ -1,16 +1,123 @@
 struct Uniforms {
+    pixel00_loc: vec3f,
+    pixel_delta_u: vec3f,
+    pixel_delta_v: vec3f,
+    camera_center: vec3f,
     time: f32,
 }
 
 @group(0) @binding(0) var<uniform> uniforms: Uniforms;
 @group(0) @binding(1) var tex: texture_storage_2d<bgra8unorm, write>;
 
-@compute @workgroup_size(1) fn cs(
+fn initRng(pixel: vec2<u32>, frame: u32) -> u32 {
+    // Adapted from https://github.com/boksajak/referencePT
+    let seed = dot(pixel, vec2<u32>(1u, 500u)) ^ jenkinsHash(frame);
+    return jenkinsHash(seed);
+}
+
+fn jenkinsHash(input: u32) -> u32 {
+    var x = input;
+    x += x << 10u;
+    x ^= x >> 6u;
+    x += x << 3u;
+    x ^= x >> 11u;
+    x += x << 15u;
+    return x;
+}
+
+fn rngNextInt(state: ptr<function, u32>) -> u32 {
+    // PCG random number generator
+    // Based on https://www.shadertoy.com/view/XlGcRh
+    let newState = *state * 747796405u + 2891336453u;
+    *state = newState;
+    let word = ((newState >> ((newState >> 28u) + 4u)) ^ newState) * 277803737u;
+    return (word >> 22u) ^ word;
+}
+
+// Construct a float with half-open range [0:1] using low 23 bits.
+// All zeroes yields 0.0, all ones yields the next smallest representable value below 1.0.
+fn float_construct_from_u32(m_in: u32) -> f32 {
+    return bitcast<f32>(m_in >> 9 | 0x3F800000u) - 1.0;
+}
+
+// Pseudo-random value in half-open range [0:1) from a f32 seed.
+fn rand(state: ptr<function, u32>) -> f32 {
+    let value = rngNextInt(state);
+    return float_construct_from_u32(value);
+}
+
+struct Ray {
+    orig: vec3f,
+    dir: vec3f,
+}
+
+fn ray_at(r: Ray, t: f32) -> vec3f {
+    return r.orig + t * r.dir;
+}
+
+fn hit_sphere(center: vec3f, radius: f32, r: Ray) -> f32 {
+    let oc = center - r.orig;
+    let a = dot(r.dir, r.dir);
+    let h = dot(r.dir, oc);
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = h * h - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - sqrt(discriminant)) / a;
+    }
+}
+
+fn ray_color(r: Ray) -> vec3f {
+    var t: f32;
+    t = hit_sphere(vec3f(0.0, 0.0, -1.0), 0.5, r);
+
+    if (t > 0.0) {
+        let n = normalize(ray_at(r, t) - vec3f(0.0, 0.0, -1.0));
+        return 0.5 * (n + 1);
+    }
+    
+    t = hit_sphere(vec3f(0.0, -100.5, -1.0), 100.0, r);
+
+    if (t > 0.0) {
+        let n = normalize(ray_at(r, t) - vec3f(0.0, -100.5, -1.0));
+        return 0.5 * (n + 1);
+    }
+    
+    let unit_dir = normalize(r.dir);
+    let a = 0.5 * (unit_dir.y + 1.0);
+    return (1.0-a)*vec3f(1.0, 1.0, 1.0) + a*vec3f(0.5, 0.7, 1.0);
+}
+
+fn sample_square(rseed: ptr<function, u32>) -> vec2f {
+    return vec2f(rand(rseed) - 0.5, rand(rseed) - 0.5);
+}
+
+fn get_ray(pixel: vec2u, rseed: ptr<function, u32>) -> Ray {
+    let offset = sample_square(rseed);
+    let pixel_sample = uniforms.pixel00_loc
+        + (uniforms.pixel_delta_u * (f32(pixel.x) + offset.x))
+        + (uniforms.pixel_delta_v * (f32(pixel.y) + offset.y));
+
+    let ray_direction = pixel_sample - uniforms.camera_center;
+    return Ray(uniforms.camera_center, ray_direction);
+}
+
+@compute @workgroup_size(64) fn cs(
     @builtin(global_invocation_id) id: vec3u
 ) {
-    const double_time = uniforms.time * 2;
+    var rseed = initRng(id.xy, bitcast<u32>(uniforms.time));
 
-    let color = vec4f(fract(vec2f(id.xy) / 32.0), abs(cos(uniforms.time)), 1);
+    let spp = 1;
+    var pixel_color = vec3f(0.0, 0.0, 0.0);
 
-    textureStore(tex, id.xy, color);
+    for (var i = 0; i < spp; i += 1) {
+        let r = get_ray(id.xy, &rseed);
+        let color = ray_color(r);
+        pixel_color += clamp(color, vec3f(0.0, 0.0, 0.0), vec3f(1.0, 1.0, 1.0));
+    }
+    pixel_color /= f32(spp);
+    
+    textureStore(tex, id.xy, vec4f(pixel_color, 1.0));
 }
