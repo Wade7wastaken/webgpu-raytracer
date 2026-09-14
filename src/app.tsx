daf56ddcfe0d01818de7f2d07745d41ff6e05876
Wade7wastaken <86390605+Wade7wastaken@users.noreply.github.com>
2026-09-13 20:15:00 -0500

    im so done with this

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,74 @@
-import { useEffect, useRef, type FC, type RefObject } from "react";
+import {
+  useEffect,
+  useRef,
+  useState,
+  type Dispatch,
+  type FC,
+  type RefObject,
+  type SetStateAction,
+} from "react";
 import { Vector3 } from "three";
 
 import computeShader from "./compute.wgsl?raw";
+
+class NonNegativeRollingAverage {
+  #total = 0;
+  #samples: number[] = [];
+  #cursor = 0;
+  readonly #numSamples;
+  public constructor(numberSamples = 30) {
+    this.#numSamples = numberSamples;
+  }
+  public addSample(v: number): void {
+    if (!(!Number.isNaN(v) && Number.isFinite(v) && v >= 0)) {
+      return;
+    }
+
+    this.#total += v - (this.#samples[this.#cursor] || 0);
+    this.#samples[this.#cursor] = v;
+    this.#cursor = (this.#cursor + 1) % this.#numSamples;
+  }
+  public get(): number {
+    return this.#total / this.#samples.length;
+  }
+}
 
 const fail = (message: string): void => {
   alert(message);
 };
 
+const useAnimationFrame = (callback: (deltaTime: number) => void): void => {
+  const requestReference = useRef(0);
+  const previousTimeReference = useRef(0);
+  const callbackReference = useRef(callback);
+
+  useEffect(() => {
+    callbackReference.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const animate = (time: DOMHighResTimeStamp): void => {
+      const deltaTime = time - previousTimeReference.current;
+      callbackReference.current(deltaTime);
+      previousTimeReference.current = time;
+      requestReference.current = requestAnimationFrame(animate);
+    };
+
+    requestReference.current = requestAnimationFrame(animate);
+
+    return (): void => {
+      cancelAnimationFrame(requestReference.current);
+    };
+  }, []);
+};
+
 const setupRenderer = async (
   canvas: HTMLCanvasElement,
   camera_pos: RefObject<Vector3>,
+  setFpsSamples: Dispatch<SetStateAction<number[]>>,
+  fpsCount: RefObject<number>,
 ): Promise<void> => {
+  console.log("starting setup");
   const adapter = await navigator.gpu.requestAdapter();
   if (adapter == undefined) {
     fail("need a browser that supports WebGPU");
@@ -49,6 +107,60 @@ const setupRenderer = async (
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
   const uniformData = new Float32Array(uniformsNumberFloats);
+
+  const triangleBufferSize = 10 * 9 * 4;
+  const triangleBuffer = device.createBuffer({
+    size: triangleBufferSize,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  });
+
+  // prettier-ignore
+  const triangleData = new Float32Array([
+    // back face
+    0, 0, -2, // bottom left
+    1, 0, -2, // bottom right
+    1, 1, -2, // top right
+
+    0, 0, -2, // bottom left
+    1, 1, -2, // top right
+    0, 1, -2, // top left
+
+    // bottom face
+    0, 0, -2, // bottom left
+    0, 0, -1, // forward left
+    1, 0, -2, // bottom right
+
+    1, 0, -2, // bottom right
+    0, 0, -1, // forward left
+    1, 0, -1, // forward right
+
+    // top face
+    0, 1, -2, // bottom left
+    1, 1, -2, // bottom right
+    0, 1, -1, // forward left
+
+    1, 1, -2, // bottom right
+    1, 1, -1, // forward right
+    0, 1, -1, // forward left
+
+    // left face
+    0, 0, -2, // bottom left
+    0, 1, -2, // bottom left
+    0, 0, -1, // forward left
+
+    0, 1, -2, // bottom left
+    0, 1, -1, // forward left
+    0, 0, -1, // forward left
+
+    // left face
+    1, 0, -2, // bottom left
+    1, 0, -1, // forward left
+    1, 1, -2, // bottom left
+
+    1, 1, -2, // bottom left
+    1, 0, -1, // forward left
+    1, 1, -1, // forward left
+  ]);
 
   const pipeline = device.createComputePipeline({
     label: "checkboard pipeline",
@@ -109,11 +221,22 @@ const setupRenderer = async (
     uniformData[14] = camera_center.z;
   };
 
+  let lastTime = 0;
+
   function render(time: DOMHighResTimeStamp): void {
     time *= 0.001;
+    const deltaTime = time - lastTime;
+    setFpsSamples((s) => {
+      const new_s = [...s];
+      new_s[fpsCount.current % 10] = 1/deltaTime;
+      return new_s;
+    });
+    fpsCount.current += 1;
+    lastTime = time;
     uniformData[15] = time;
     setUniforms(uniformData, canvas.clientWidth, canvas.clientHeight);
     device.queue.writeBuffer(uniformBuffer, 0, uniformData);
+    device.queue.writeBuffer(triangleBuffer, 0, triangleData);
 
     // Get the current texture from the canvas context
     const canvasTexture = context.getCurrentTexture();
@@ -123,6 +246,7 @@ const setupRenderer = async (
       entries: [
         { binding: 0, resource: { buffer: uniformBuffer } },
         { binding: 1, resource: canvasTexture.createView() },
+        { binding: 2, resource: { buffer: triangleBuffer } },
       ],
       label: "bindGroup0",
     });
@@ -137,7 +261,7 @@ const setupRenderer = async (
     const commandBuffer = encoder.finish();
     device.queue.submit([commandBuffer]);
 
-    requestAnimationFrame(render);
+    // requestAnimationFrame(render);
   }
 
   requestAnimationFrame(render);
@@ -145,14 +269,18 @@ const setupRenderer = async (
 
 const App: FC = () => {
   const canvasReference = useRef<HTMLCanvasElement>(null);
-  const camera_pos = useRef<Vector3>(new Vector3(0, 0, 0));
+  const camera_pos = useRef<Vector3>(new Vector3(0.5, 0.5, 0));
+  const [fpsSamples, setFpsSamples] = useState(
+    Array.from<number>({ length: 10 }),
+  );
+  const fpsCount = useRef(0);
 
   useEffect(() => {
     if (canvasReference.current == undefined) {
       fail("couldn't find canvas");
       return;
     }
-    void setupRenderer(canvasReference.current, camera_pos);
+    void setupRenderer(canvasReference.current, camera_pos, setFpsSamples, fpsCount);
   }, []);
 
   useEffect(() => {
@@ -187,12 +315,15 @@ const App: FC = () => {
   }, []);
 
   return (
-    <canvas
-      ref={canvasReference}
-      className="block"
-      width={(720 * 19) / 9}
-      height={720}
-    ></canvas>
+    <>
+      <canvas
+        ref={canvasReference}
+        className="block"
+        width={(720 * 19) / 9}
+        height={720}
+      ></canvas>
+      <p>{(fpsSamples.reduce((a, b) => a + b) / 10).toFixed(2)}</p>
+    </>
   );
 };
 
--- src/compute.wgsl
+++ src/compute.wgsl
@@ -8,6 +8,7 @@ struct Uniforms {
 
 @group(0) @binding(0) var<uniform> uniforms: Uniforms;
 @group(0) @binding(1) var tex: texture_storage_2d<bgra8unorm, write>;
+@group(0) @binding(2) var<storage, read> vert: array<f32>;
 
 fn initRng(pixel: vec2<u32>, frame: u32) -> u32 {
     // Adapted from https://github.com/boksajak/referencePT
@@ -28,10 +29,16 @@ fn jenkinsHash(input: u32) -> u32 {
 fn rngNextInt(state: ptr<function, u32>) -> u32 {
     // PCG random number generator
     // Based on https://www.shadertoy.com/view/XlGcRh
-    let newState = *state * 747796405u + 2891336453u;
-    *state = newState;
-    let word = ((newState >> ((newState >> 28u) + 4u)) ^ newState) * 277803737u;
-    return (word >> 22u) ^ word;
+    // let newState = *state * 747796405u + 2891336453u;
+    // *state = newState;
+    // let word = ((newState >> ((newState >> 28u) + 4u)) ^ newState) * 277803737u;
+    // return (word >> 22u) ^ word;
+
+    *state = *state * 747796405u + 2891336453u;
+    // *state = newState;
+    return *state;
+    // let word = ((newState >> ((newState >> 28u) + 4u)) ^ newState) * 277803737u;
+    // return (word >> 22u) ^ word;
 }
 
 // Construct a float with half-open range [0:1] using low 23 bits.
@@ -51,43 +58,118 @@ struct Ray {
     dir: vec3f,
 }
 
-fn ray_at(r: Ray, t: f32) -> vec3f {
+fn ray_at(r: ptr<function, Ray>, t: f32) -> vec3f {
     return r.orig + t * r.dir;
 }
 
-fn hit_sphere(center: vec3f, radius: f32, r: Ray) -> f32 {
-    let oc = center - r.orig;
-    let a = dot(r.dir, r.dir);
-    let h = dot(r.dir, oc);
-    let c = dot(oc, oc) - radius * radius;
-    let discriminant = h * h - a * c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - sqrt(discriminant)) / a;
-    }
+fn getVertex(index: u32) -> vec3f {
+    let i = index * 3u;
+    return vec3f(vert[i], vert[i + 1u], vert[i + 2u]);
 }
 
-fn ray_color(r: Ray) -> vec3f {
-    var t: f32;
-    t = hit_sphere(vec3f(0.0, 0.0, -1.0), 0.5, r);
+struct HitRecord {
+    p: vec3f,
+    n: vec3f,
+    t: f32,
+}
 
-    if (t > 0.0) {
-        let n = normalize(ray_at(r, t) - vec3f(0.0, 0.0, -1.0));
-        return 0.5 * (n + 1);
-    }
-    
-    t = hit_sphere(vec3f(0.0, -100.5, -1.0), 100.0, r);
+fn hit_triangle(r: ptr<function, Ray>, triIdx: u32, max_t: f32, outHitRec: ptr<function, HitRecord>) -> bool {
+    let a = getVertex(triIdx * 3);
+    let b = getVertex(triIdx * 3 + 1);
+    let c = getVertex(triIdx * 3 + 2);
 
-    if (t > 0.0) {
-        let n = normalize(ray_at(r, t) - vec3f(0.0, -100.5, -1.0));
-        return 0.5 * (n + 1);
+    let edge1 = b - a;
+    let edge2 = c - a;
+    let outward_normal = normalize(cross(edge1, edge2));
+
+    // --- stuff we can't do before ---
+
+    let ray_cross_e2 = cross(r.dir, edge2);
+    let denom = dot(edge1, ray_cross_e2);
+
+    if (abs(denom) < 1e-8) {
+        return false;
     }
+
+    let s = r.orig - a;
+    let u = dot(s, ray_cross_e2) / denom;
+
+    if (u < 0.0 || u > 1.0) {
+        return false;
+    }
+
+    let s_cross_e1 = cross(s, edge1);
+    let v = dot(r.dir, s_cross_e1) / denom;
+    if (v < 0.0 || u + v > 1.0) {
+        return false;
+    }
+
+    let t = dot(edge2, s_cross_e1) / denom;
+
+    if (t < 0.001 || t > max_t) {
+        return false;
+    }
+
+    let p = ray_at(r, t);
+
+    outHitRec.p = p;
+    outHitRec.n = outward_normal;
+    outHitRec.t = t;
     
-    let unit_dir = normalize(r.dir);
-    let a = 0.5 * (unit_dir.y + 1.0);
-    return (1.0-a)*vec3f(1.0, 1.0, 1.0) + a*vec3f(0.5, 0.7, 1.0);
+    return true;
+}
+
+fn randNormalVector(state: ptr<function, u32>) -> vec3<f32> {
+    let r = pow(rand(state), 0.33333f);
+    let cosTheta = 1f - 2f * rand(state);
+    let sinTheta = sqrt(1f - cosTheta * cosTheta);
+    let phi = 2f * 3.1415926535 * rand(state);
+
+    let x = r * sinTheta * cos(phi);
+    let y = r * sinTheta * sin(phi);
+    let z = cosTheta;
+
+    return vec3(x, y, z);
+}
+
+fn ray_color(initialRay: Ray, state: ptr<function, u32>) -> vec3f {
+    var ray = initialRay;
+
+    var rayColor = vec3f(1.0, 1.0, 1.0);
+
+    var hitRec = HitRecord();
+    var min_t = 0; // double check
+
+    let far_away = 10000000f;
+
+    for (var bounce = 0u; bounce < 30; bounce += 1) {
+
+        var max_t = far_away;
+    
+        // don't call arrayLength here
+        for (var triIdx = 0u; triIdx < arrayLength(&vert) / 9; triIdx += 1) {
+            if hit_triangle(&ray, triIdx, max_t, &hitRec) {
+                max_t = hitRec.t;
+            }
+        }
+
+        // we found an intersection closer than `far_away`
+        if max_t < far_away {
+            var scatter_dir = hitRec.n + randNormalVector(state);
+            if (abs(scatter_dir.x) < 1e-6 && abs(scatter_dir.y) < 1e-6 && abs(scatter_dir.z) < 1e-6) {
+                scatter_dir = hitRec.n;
+            }
+            rayColor *= vec3f(0.5, 0.5, 0.5);
+            ray = Ray(hitRec.p, scatter_dir);
+        } else {
+            let unit_dir = normalize(ray.dir);
+            let a = 0.5 * (unit_dir.y + 1.0);
+            // return vec3f(0.3, 0.4, 0.5);
+            return rayColor * (1.0-a)*vec3f(1.0, 1.0, 1.0) + a*vec3f(0.3, 0.7, 1.0);
+        }
+    }
+
+    return vec3f(0.0, 0.0, 0.0);
 }
 
 fn sample_square(rseed: ptr<function, u32>) -> vec2f {
@@ -104,17 +186,17 @@ fn get_ray(pixel: vec2u, rseed: ptr<function, u32>) -> Ray {
     return Ray(uniforms.camera_center, ray_direction);
 }
 
-@compute @workgroup_size(64) fn cs(
+@compute @workgroup_size(1) fn cs(
     @builtin(global_invocation_id) id: vec3u
 ) {
     var rseed = initRng(id.xy, bitcast<u32>(uniforms.time));
 
-    let spp = 1;
+    let spp = 10;
     var pixel_color = vec3f(0.0, 0.0, 0.0);
 
     for (var i = 0; i < spp; i += 1) {
         let r = get_ray(id.xy, &rseed);
-        let color = ray_color(r);
+        let color = ray_color(r, &rseed);
         pixel_color += clamp(color, vec3f(0.0, 0.0, 0.0), vec3f(1.0, 1.0, 1.0));
     }
     pixel_color /= f32(spp);
